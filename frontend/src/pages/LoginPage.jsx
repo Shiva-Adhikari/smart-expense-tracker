547cb5e6e29e52a807dee432d1bf0c400ea29ec5
Shiva-Adhikari <s2whl6xh@duck.com>
2025-11-03 14:05:45 +0545

    feat: successfully add bootstrap in react but not added backend

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+function LoginPage() {
+    return (
+        <>
+            <h1>welcome to Login page</h1>
+        </>
+    )
+}
+
+export default LoginPage
